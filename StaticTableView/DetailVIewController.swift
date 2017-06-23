22e3da2bcb9893a5d207e1abae81a062bf98c106
Bansi Bhatt <bannsi.bhatt07@gmail.com>
2017-06-23 12:20:33 +0530

    Added Another View

---
+++ StaticTableView/DetailVIewController.swift
@@ -0,0 +1,24 @@
+//
+//  DetailVIewController.swift
+//  StaticTableView
+//
+//  Created by Bansi Bhatt on 23/06/17.
+//
+//
+
+import Foundation
+import UIKit
+
+class DetailViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
